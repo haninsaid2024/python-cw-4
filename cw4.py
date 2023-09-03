7d5377b9519b6d7be946c6714e1099d55c86d7f9
haninsaid2024 <haninfouda077@gmail.com>
2023-09-03 14:52:54 +0300

    Create cw4.py

---
+++ cw4.py
@@ -0,0 +1,16 @@
+ def my_name():
+  print("my name is haneen")
+  my_name()
+def my_meal(food,drink):
+   
+   print(f"I like to eat {food} and while drinking{drink}") 
+   my_meal(pizza,cola)
+
+def cude(number)
+  return number **3
+def by_three(number):
+  if number  %3 == 0:
+ return cude(number)
+else: 
+return "false :("
+print(by_three(1))
